817f2929f84ac722bb1a43847672a945be4aff86
Jason Mei <jason.mei403@gmail.com>
2018-07-10 05:04:53 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,37 @@
 class Triangle
   # write code here
+  attr_accessor :side1, :side2, :side3
+
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
+
+  def kind
+    if side1 < 1 || side2 < 1 || side3 < 1
+      begin
+        raise TriangleError
+      rescue TriangleError => error
+          puts error.message
+      end
+
+    if side1 == side2 && side2 == side3
+      :equilateral
+    elsif side1 == side2 || side1 == side3 || side2 == side3
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  def triangle_inequality?
+    !((side1 + side2) > side3 || (side1 + side3) > side2 || (side2 + side3) > side1)
+  end
+
+  class TriangleError < StandardError
+    def message
+      "The sum of the lengths of any two sides of a triangle always exceeds the length of the third side. Further, each side must be larger than 0."
+    end
+  end
 end
